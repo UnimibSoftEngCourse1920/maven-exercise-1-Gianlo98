8d27186d2ebe73d28ce7512ea9e0d7dd4c372931
Gianlo <gianlorenzo.occhipinti@gmail.com>
2019-10-29 10:21:07 +0100

    Added and tested maven

---
+++ src/main/java/com/unimib/lab3es1/quickstart/App.java
@@ -0,0 +1,13 @@
+package com.unimib.lab3es1.quickstart;
+import com.google.gson.Gson;
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	
+    public static void main( String[] args ){
+        Gson gson = new Gson();
+        System.out.println( "Hello World!" );
+    }
+}
